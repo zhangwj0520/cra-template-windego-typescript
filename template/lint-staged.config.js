820307dd1c243989a1302b658a1574c50c3db0e8
张为杰 <zhangwj0520@hotmail.com>
2020-04-19 09:07:58 +0800

    修改错误

--- template/lint-staged.config.js
+++ template/lint-staged.config.js
@@ -1,6 +1,6 @@
-{
-  "*.json":"prettier --write",
-  "*.{ts,tsx}": ["eslint --cache --fix","prettier --write"],
+module.exports = {
+  "*.json": "prettier --write",
+  "*.{ts,tsx}": ["eslint --cache --fix", "prettier --write"],
   "*.css": ["stylelint --cache --fix", "prettier --write"],
   "*.scss": ["stylelint --cache --syntax scss --fix", "prettier --write"],
 }
